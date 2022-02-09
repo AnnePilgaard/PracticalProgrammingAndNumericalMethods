ac8ae25c1aaa990c21c3883c802fa404d982bf28
Anne <anneprasmussen@gmail.com>
2022-02-09 10:41:20 +0100

    hello

---
+++ exercises/math/main.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+
+public static class main{
+    static void Main(){
+        double sqrt2 = Sqrt(2.0);
+        Write($"Sqrt of 2 = {sqrt2} and square of sqrt2 is {sqrt2*sqrt2}\n");
+
+    }
+
+
+}
--- lectures/3/main.cs
+++ lectures/3/main.cs
@@ -1,22 +1,29 @@
 using System;
 using static System.Console;
 
-public static class Math{
+public static class test{
     public static int n = 7;
     public static const double pi = 3.1415927;
     public static const double e = 2.71828;
 
 }
 
-public static class Main{
+public static class main{
+    static string s = "main\n"
     static void hello(){
         string s = "hello\n";
         Write(s);
+        {
+            string s1 = "block\n"
+            Write(s1)
         }
-    static int main{
-        double x;
-        x = math.pi;
-        y = math.e;
+
+    }
+
+    static int Main{
+        double x, y;
+        x = test.pi;
+        y = test.e;
         Write($'x={x} y={y}\n')
     return 0;
     }
