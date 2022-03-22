791adf7cfa9b3426f992de6b585a9dbafe270ccd
Anne <anneprasmussen@gmail.com>
2022-03-22 17:08:04 +0100

    update

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -1,11 +1,18 @@
-using System;
 using static System.Console;
 using static System.Math;
-
-
-public static class main{
-    static void Main(){
-
-    }
-
+using static cmath;
+class main{
+public static void Main(){
+	complex i = I;
+	WriteLine($"imaginary i = {i}\n");
+	
+	exp(I).print("exp(I)         =");
+	(Cos(1)+I*Sin(1)).print("cos(1)+i sin(1)=");
+	log(exp(I)).print("\nln(exp(I))=");
+	exp(I*PI).print("\nexp(I*PI)=");
+	log(I).print("\nlog(I)=");
+	//(PI*I/2).print("I*PI/2=");
+	(I.pow(I)).print("\nI^I          =");
+	exp(I*log(I)).print("exp(I*log(I))=");
+}
 }
