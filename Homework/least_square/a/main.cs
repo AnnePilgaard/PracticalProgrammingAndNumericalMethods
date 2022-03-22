081a4bdee10d071c4e507c40bf9fe5c85f615951
Anne <anneprasmussen@gmail.com>
2022-03-22 12:58:38 +0100

    least_square_a

---
+++ Homework/least_square/a/main.cs
@@ -0,0 +1,141 @@
+using System;
+using System.IO;
+using static System.Console;
+using static System.Math;
+
+public static class main{
+
+
+    static (vector, vector, vector) readData(string filename){
+
+        string[] lines = File.ReadAllLines(filename);  
+
+        int nrows = lines.Length;
+
+        string[] firstRow = lines[0].Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        int ncolumns = firstRow.Length; 
+
+        vector x = new vector(nrows);
+        vector y = new vector(nrows);
+        vector dy = new vector(nrows);
+
+        int idx = 0;
+ 
+
+        foreach(string line in lines){  
+            string[] dataarray = line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            x[idx] = double.Parse(dataarray[0]);
+            y[idx] = double.Parse(dataarray[1]);
+            dy[idx] = double.Parse(dataarray[2]);
+            idx++;
+
+        }
+
+        return (x, y, dy);
+    }
+
+
+    static void printVector(vector b){
+
+        int n = b.size;
+
+        for (int i = 0; i < n; i++){
+            WriteLine($"{b[i]}");
+        }
+        WriteLine("\n");
+    }
+    
+
+    static (vector, matrix) lsfit(Func<double, double>[] fs, vector x, vector y, vector dy){
+
+        int n = x.size;
+        int m = fs.Length;
+
+        matrix A = new matrix(n, m);
+        vector b = new vector(n);
+
+        for(int i = 0; i < n; i++){
+            b[i] = y[i] / dy[i];
+            for(int k = 0; k < m; k++){
+                A[i,k] = fs[k](x[i]) / dy[i];
+            }
+        }
+        var qra = new QRGS(A);
+        vector c = qra.solve(b);
+        matrix invA = qra.inverse();
+        var S = invA*invA.T;
+
+        return (c, S);
+    }
+
+    static void Main(){
+
+        
+        var dataVectors = readData("data.txt");
+        vector x = dataVectors.Item1;
+        vector y = dataVectors.Item2;
+        vector dy = dataVectors.Item3;
+
+        //printVector(x);
+        //printVector(y);
+        //printVector(dy);
+
+        var fs = new Func<double,double>[] { z => 1.0, z => z};
+
+        vector logY = new vector(y.size);
+        vector logdY = new vector(y.size);
+
+        for (int i = 0; i < y.size; i++){
+            logY[i] = Log(y[i]);
+            logdY[i] = Log(dy[i]);
+        }
+
+        try
+        {
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("dataLog.txt");
+            //Write text
+            for(int j = 0; j < y.size; j++){
+                sw.WriteLine($"{x[j]} {logY[j]} {logdY[j]}");
+            }
+            //Close the file
+            sw.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+
+        var fitTuple = lsfit(fs, x,logY, logdY);
+
+        vector c = fitTuple.Item1;
+
+        printVector(c);
+
+        double c0 = c[0];
+        double c1 = c[1];
+
+        try
+        {
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("fitPlot.txt");
+            //Write text
+            for(double z = 0; z <= 15 ;z+=1.0/8){
+                double yfit =  c0 + c1*z;
+                sw.WriteLine($"{z} {yfit}");
+            }
+            //Close the file
+            sw.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+
+        WriteLine($"The half life of ThX from the fit is {Log(2)/Abs(c1)} days, while the half-life of 224Ra from Google is 3.6 days");
+
+
+    }
+}
