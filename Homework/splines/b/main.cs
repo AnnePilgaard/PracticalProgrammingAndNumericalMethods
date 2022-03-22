cf121b73f8cab76daa9447a831b0c4dfb0e29b14
Anne <anneprasmussen@gmail.com>
2022-03-22 22:53:28 +0100

    splines_b

--- Homework/splines/b/main.cs
+++ Homework/splines/b/main.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using static System.Console;
 using static System.Math;
-using qSpline;
+
 public static class main{
     static void Main(string[] args){
 
@@ -19,7 +19,54 @@ public static class main{
             idx++;
         }
 
-        qspline qSpline1 = qspline(xarray, y);
+
+       
+        qspline qSpline1 = new qspline(xarray, yarray);
+
+        
+        try
+        {
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("qspline1.txt");
+            //Write text
+            for(double z = xarray[0]; z <= xarray[xarray.Length-1]; z += 1.0/8){
+                double yz = qSpline1.spline(z);
+                sw.WriteLine($"{z} {yz}");
+            }
+            //Close the file
+            sw.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+
+        try
+        {
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("qsplineDer.txt");
+            //Write text
+            for(double z = xarray[0]; z <= xarray[xarray.Length-1]; z += 1.0/8){
+                double yz = qSpline1.derivative(z);
+                sw.WriteLine($"{z} {yz}");
+            }
+            //Close the file
+            sw.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+
+        double integ3 = qSpline1.integral(3.0);
+        WriteLine($"The integration of the spline from 0 to 3 is {integ3}");
+        double integ5 = qSpline1.integral(5.0);
+        WriteLine($"The integration of the spline from 0 to 5 is {integ5}");
+        double integ8 = qSpline1.integral(8.0);
+        WriteLine($"The integration of the spline from 0 to 8 is {integ8}");
+        
 
 
     }
--- Homework/splines/b/qSpline.cs
+++ Homework/splines/b/qSpline.cs
@@ -4,62 +4,81 @@ using static System.Math;
 
 
 public class qspline {
-	double[] x,y,b,c;
+	public readonly double[] x,y,b,c;
 	public qspline(double[] xs,double[] ys){
-
-		int n  = xs.GetLength(0);
-
-		double[] bs = new double[n];
-		double[] cs = new double[n];
-
-		
-		//Forward recursion: initial assumption
-		double dxi0 = x[1] - x[0];
-		double dyi0 = y[1] - y[0];
-		bs[0] = dyi0/dxi0;
-
-		cs[0] = 0;
-
-		//Forward resurcion loop:
-		for(int i = 1; i < n; i++){
-			double dxi = xs[i+1] - xs[i];
-			double dyi = ys[i+1] - ys[i];
-			bs[i] = dyi/dxi;
-
-			double dbi_1 = b[i-1]-b[i];
-			double dxi_1 = xs[i-1] -xs[i];
-
-			cs[i] = (-dbi_1 +  cs[i-1] + dxi_1)/dxi;
-		}
-
-		//Backward rescursion
-
-		
-		double[] bs2 = new double[n];
-		double[] cs2 = new double[n];
-
-		cs2[n-1] = 0.5*cs[n-1];
-		bs2[n-1] = xs[n-1] - xs[n-2];
-
-
-		for(int i = n-2; i > n; i--){
-			double dxi = xs[i+1] - xs[i];
-			double dyi = ys[i+1] - ys[i];
-			bs2[i] = dyi/dxi;
-
-			double dbi = b[i+1]-b[i];
-			double dxi_1 = xs[i+2] -xs[i+1];
-
-			cs2[i] = (dbi - cs[i+1] + dxi_1)/dxi;
-		}
+		int n = xs.Length;
 
 		x = xs;
 		y = ys;
-		b = bs2;
-		c = cs2;
+		b = new double[n-1];
+		c = new double[n-1];
+
+		double[] h = new double[n-1];
+		double[] p = new double[n-1];
+
+		//Create h = x[i+1] - x[i] and p = (y[i+1] - y[i])/h[i] arrays
+		for( int i = 0; i < n-1; i++){
+			h[i] = x[i+1] - x[i];
+			p[i] = (y[i+1] - y[i])/h[i];
 		}
-	//public double spline(double z){/* evaluate the spline */}
-	//public double derivative(double z){/* evaluate the derivative */}
-	//public double integral(double z){/* evaluate the integral */}
+
+		//Forward recursion
+		c[0] = 0;
+		for (int i = 0; i < n-2; i++){
+			c[i+1] = (p[i+1] - p[i] - c[i]*h[i])/h[i+1];
+		}
+
+		//Backward recursion
+		c[n-2] = c[n-2]/2;
+		for (int i = n-3; i >= 0; i--){
+			c[i] = (p[i+1] - p[i] - c[i+1]*h[i+1])/h[i];
+		}
+
+		//Set bs
+		for (int i = 0; i < n-1; i++){
+			b[i] = p[i] - c[i]*h[i];
+		}
+
+		}
+	public double spline(double z){
+		int i=binsearch(x,z);
+		return y[i]+b[i]*(z-x[i])+c[i]*(z-x[i])*(z-x[i]);
+		}
+
+	public static int binsearch(double[] x, double z){/* locates the interval for z by bisection */ 
+        if(!(x[0]<=z && z<=x[x.Length-1])) throw new Exception("binsearch: bad z");
+        int i=0, j=x.Length-1;
+        while(j-i>1){
+            int mid=(i+j)/2;
+            if(z>x[mid]) i=mid; else j=mid;
+            }
+        return i;
+	}
+	public double derivative(double z){
+		int i=binsearch(x,z);
+		return b[i] + 2*c[i]*(z-x[i]);
+		
+		}
+	public double integral(double z){
+		int i=binsearch(x,z);
+		double integSum  = 0;
+
+		//Do the inegration in every interval before the interval in which z is located 
+		for (int j = 0; j < i; j++){
+
+			double integ = y[i]*(x[i+1]-x[i]) + 1/2*b[i]*(x[i+1]*x[i+1]-x[i]*x[i]) - b[i]*x[i]*(x[i+1]-x[i]) 
+							+ 1/3*c[i]*(x[i+1]*x[i+1]*x[i+1]-x[i]*x[i]*x[i]) -c[i]*(x[i+1]*x[i+1]-x[i]*x[i])*x[i]
+							+ c[i]*x[i]*x[i]*(x[i+1]-x[i]);
+			integSum += integ;
+		}
+	
+		double integFinal = y[i]*(z-x[i]) + 1/2*b[i]*(z*z-x[i]*x[i]) - b[i]*x[i]*(z-x[i]) 
+							+ 1/3*c[i]*(z*z*z-x[i]*x[i]*x[i]) -c[i]*(z*z-x[i]*x[i])*x[i]
+							+ c[i]*x[i]*x[i]*(z-x[i]);
+		integSum += integFinal;
+
+		return integSum;
+
+		}
 
 }
